f1acd05056309059dd3f6b0e838c27df6f8ac385
sanderhd <sanderhoudijk10@gmail.com>
2026-01-15 22:03:49 +0100

    feat: start on the creating mechanism

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-const { select } = require('@inquirer/prompts');
+const prompts = require('@inquirer/prompts')
 const chalk = require('chalk');
+const { spawn } = require('child_process');
 
 (async () => {
     const banner = chalk.blue.bold(`
@@ -11,7 +12,7 @@ const chalk = require('chalk');
 
     console.log(banner);
 
-    const feature = await select({
+    const feature = await prompts.select({
         message: 'Select starter:',
         choices: [
             { name: 'NextJS Starter', value: 'nextjs' },
@@ -20,5 +21,38 @@ const chalk = require('chalk');
         ],
     });
 
-    console.log('Selected:', feature);
+    if (feature === 'nextjs') {
+        const appName = await prompts.input({
+            message: 'Enter a name for your Next.JS app:',
+            initial: 'my-next-app', 
+        })
+
+        console.log(`${new Date().toLocaleTimeString()} ${chalk.blue(`INFO:`)} Creating Next.JS app: ${appName}\n`);
+
+        const args = [
+            'create-next-app@latest',
+            appName,
+            '--typescript',
+            '--eslint',
+            '--tailwind',
+            '--app',
+            '--src-dir',
+            '--import-alias', '@/*',
+            '--no-turbopack'
+        ];
+
+        const child = spawn('npx', args, {
+            stdio: 'inherit',
+            shell: true
+        });
+
+        child.on('exit', (code) => {
+            if (code === 0) {
+                console.log(chalk.green(`\n✓ Next.JS app "${appName}" is ready!`));
+                console.log(chalk.blue(`\nTo get started:\n  cd ${appName}\n  npm run dev`));
+            } else {
+                console.log(chalk.red(`\n✗ Failed to create Next.JS app. Exit code ${code}`))
+            }
+        })
+    }
 })();
